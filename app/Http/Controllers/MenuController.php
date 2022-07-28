0d85cc623c57796e70a7ce821fd907f6a7ef383b
majiidmuhammad <majiid831@gmail.com>
2022-07-28 21:58:29 +0700

    setup tampilan manajer di bagian menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    public function index(){
+
+        $data = menu::all();
+        return view('', compact('data'));
+    }
+}
