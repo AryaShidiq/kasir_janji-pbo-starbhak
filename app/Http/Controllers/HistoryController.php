a29661d6afeaf6a17c27ceeb3e6f2f03a6484681
AryaShidiq <aryashidiq30@gmail.com>
2022-07-29 09:08:34 +0700

    Buat function CRUD tahap 1

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\History;
 use Illuminate\Http\Request;
 
 class HistoryController extends Controller
 {
-    //
+    public function indexhistory()
+    {
+        $history = history::all();
+        return view('history.index', compact('history'));
+    }
+    public function crthistory()
+    {
+        return view('history.create');
+    }
+    public function strhistory(Request $request)
+    {
+        history::create($request->all());
+        return redirect('/history');
+    }
+    public function edithistory($id)
+    {
+        $history = History::find($id);
+        return view('history.edit', compact('history'));
+    }
+    public function updthistory(Request $request,$id)
+    {
+        $history = History::find($id);
+        $history->update($request->all());
+        return redirect('/history');
+    }
+    public function dlthistory($id)
+    {
+        $history = History::find($id);
+        $history->delete();
+        return redirect('/history');
+    }
 }
