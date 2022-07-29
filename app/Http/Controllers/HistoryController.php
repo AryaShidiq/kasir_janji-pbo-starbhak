8b5a3b3dc1e79ea280a2eba0bdb65386f60281ba
majiidmuhammad <majiid831@gmail.com>
2022-07-29 09:12:34 +0700

    setup tampian

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\History;
 use Illuminate\Http\Request;
 
 class HistoryController extends Controller
 {
-    //
+
 }
--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pegawai;
 use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    //
+   
 }
