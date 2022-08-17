ed01eef3c15c68b4b4dc393fb372407f293d590d
majiidmuhammad <majiid831@gmail.com>
2022-08-17 18:39:43 +0700

    tambah A.P.I Menu

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 //posts
 Route::apiResource('/kategori', App\Http\Controllers\Api\KategoriController::class);
+
+//Menu
+Route::apiResource('/menu', App\Http\Controllers\Api\MenuController::class);
